f179b69156f8854cfb8982007c264ce821ba186e
Hassen Dhaouadi <Dhaouadi002@gmail.com>
2023-09-08 00:43:38 +0300

    change port from 3000 to 8080 as mentioned in the document

--- src/helpers/constant/global.constant.js
+++ src/helpers/constant/global.constant.js
@@ -1,6 +1,5 @@
-global.port = process.env.POKEMON_GO_SERVER_PORT || 3000;
+global.port = process.env.POKEMON_GO_SERVER_PORT || 8080;
 global.buildVersion = '1.0.0';
 global.serverStartTime = new Date();
-global.apiVersion = process.env.POKEMON_GO_API_VERSION || 'v1';
 global.environment = process.env.POKEMON_GO_ENVIRONNEMENT || 'DEV';
 global.limit = 10;
