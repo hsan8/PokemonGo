3bda2463d0016cedf2d30c07353753276692b6e1
Hassen Dhaouadi <Dhaouadi002@gmail.com>
2023-09-08 00:45:17 +0300

    require all routes under version v1.0.0

---
+++ src/helpers/routes/v1.0.0/routes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const router = express.Router();
+
+// all routes that belong to version v1.0.0.0
+router.use('/pokemon', require('../../../v1.0.0/pokemon/routes/pokemon.route'));
+
+module.exports = router;
