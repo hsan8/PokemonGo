01fdf3c770df97c7c1f89a1ada862e3630cad92c
Hassen Dhaouadi <Dhaouadi002@gmail.com>
2023-09-09 15:56:23 +0300

    add : no-restricted-globals

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
   extends: ['airbnb/base', 'plugin:node/recommended'],
   rules: {
     'linebreak-style': 0,
+    'no-restricted-globals': ['error', 'event', 'fdescribe'],
     'no-unused-vars': 'warn',
     'no-console': 'off',
     'no-new': 0,
