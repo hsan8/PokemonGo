831acdbc80408009f47f2a8ee480b7205983e02c
Hassen Dhaouadi <Dhaouadi002@gmail.com>
2023-09-09 15:57:32 +0300

    update version in comments

--- src/helpers/routes/v1.0.0/routes.js
+++ src/helpers/routes/v1.0.0/routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-// all routes that belong to version v1.0.0.0
+// all routes that belong to version v1.0.0
 router.use('/pokemon', require('../../../v1.0.0/pokemon/routes/pokemon.route'));
 
 module.exports = router;
