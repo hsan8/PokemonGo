9c07e44941f4752af9ced5cb916e7bbd8cff4be5
Hassen Dhaouadi <Dhaouadi002@gmail.com>
2023-09-10 22:54:56 +0300

    add CRUD routes

--- src/v1.0.0/pokemon/routes/pokemon.route.js
+++ src/v1.0.0/pokemon/routes/pokemon.route.js
@@ -1,8 +1,30 @@
 const express = require('express');
-const { listAllPokemon, seedPokemonExcel } = require('../controllers/pokemon.controller');
+const {
+  listAllPokemon,
+  seedPokemonExcel,
+  createPokemon,
+  getPokemonByObjectID,
+  updatePokemon,
+  deletePokemonByObjectID
+} = require('../controllers/pokemon.controller');
+const {
+  validatePokemonPayload,
+  validateIsValidID,
+  validateListingQuery
+} = require('../middleware/pokemon.middleware');
 
 const router = express.Router();
-router.get('/listAllPokemon', listAllPokemon);
+
 router.post('/seed', seedPokemonExcel);
+router.get('/listAllPokemon', validateListingQuery, listAllPokemon);
+router.post('/create', validatePokemonPayload, createPokemon);
+router.get('/getByID/:pokemonId', validateIsValidID, getPokemonByObjectID);
+router.put(
+  '/update/:pokemonId',
+  validateIsValidID,
+  validatePokemonPayload,
+  updatePokemon
+);
+router.delete('/deleteByID/:pokemonId', validateIsValidID, deletePokemonByObjectID);
 
 module.exports = router;
